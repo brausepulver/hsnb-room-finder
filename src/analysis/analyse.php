0b1c0617bcbd519b74fb994f0d2302d689fc91ae
flooo000 <62400151+flooo000@users.noreply.github.com>
2020-04-05 22:46:50 +0200

    start analyse.php

--- src/analysis/analyse.php
+++ src/analysis/analyse.php
@@ -1,19 +1,30 @@
-<?php
+<?php declare(strict_types=1);
 
-declare(strict_types=1);
+require_once "../Import/Importer.php";
+require_once "../Import/utility/Event.php";
+require_once "../Import/utility/Room.php";
+require_once "../Import/utility/TimeVector.php";
 
 use Import\Importer;
+use Import\Importer\{Event, Room, TimeVector};
+
+// $load = spl_autoload("Importer.php");
 
 $start = new DateTime("Monday next week 08:00:00");
 $end = new DateTime("Monday next week 10:00:00");
 
-$times = Importer::query($start, $end);
+$timevector = Importer::query($start, $end);
 
-$dayCounter = $start;
+//eine methode -> analyse für einen tag
+//schauen wie viele Tage -> methode so oft durchführen
 
-while ($dayCounter < $end) {
+// $minTime = 60 -> 4 mal
+
+$times = $timevector->getAll();
+print_r($times[0][4]->id2);
+
+for($i = 0; $i < count($times); $i++){
+    
 
 
-
-    $dayCounter->add(new DateInterval('P1D'));
 }
