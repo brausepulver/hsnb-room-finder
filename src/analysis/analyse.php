e3e67788321770ef6733a2e590f1926079834163
flooo000 <62400151+flooo000@users.noreply.github.com>
2020-04-10 12:56:35 +0200

    first draft: one day analysis
    added methods to analyse one day
    there is still a problem at the end

--- src/analysis/analyse.php
+++ src/analysis/analyse.php
@@ -5,44 +5,128 @@ require_once "../Import/utility/Event.php";
 require_once "../Import/utility/Room.php";
 require_once "../Import/utility/TimeVector.php";
 
+// use Import\Importer;
+// use Import\Importer\{Event, Room, TimeVector};
+
+require_once('../Import/Importer.php');
 use Import\Importer;
-use Import\Importer\{Event, Room, TimeVector};
 
-// $load = spl_autoload("Importer.php");
 
-//later values GUI interaction
-$start = new DateTime("Monday next week 08:00:00");
-$end = new DateTime("Monday next week 10:00:00");
-$minTime = 60;
+class FreeRooms{
 
-function getIDarray(DateTime $start, DateTime $end){
+     private $id;
+     private $start;
+     private $end;
 
-    $timevector = Importer::query($start, $end);
+     private function __construct(int $id, int $start, int $end){
 
-    //eine methode -> analyse für einen tag
-    //schauen wie viele Tage -> methode so oft durchführen
-    
-    // $minTime = 60 -> 4 mal
-    
-    $times = $timevector->getAll();
-    // print_r($times[0][4]->id2);
-    $arrayID;
-    for($i = 0; $i < count($times); $i++){
-        $times[$i];                         //Uhrzeit [0]=8:00
-        for($j = 0; $j < count($times[$i]); $j++){
-            $arrayID[$i][$j] = $times[$i][$j]->id2;
-        }
-        // asort($arrayID[$i]);
-        // print_r($arrayID[$i]);
+        $this->$id = $id;
+        $this->start = $start;
+        $this->end = $end; 
     }
-    print_r($arrayID);
+
+
 }
 
-getIDarray($start, $end);
 
+//gets time interval
+//puts every room id2 in an array
+function getIDarray(DateTime $start, DateTime $end){
+    $timevector = Importer::query($start, $end);
+ 
+    $times = $timevector->getAll();
+    $timeID;
+    for($i = 0; $i < count($times); $i++){
+        $times[$i];                         
+        for($j = 0; $j < count($times[$i]); $j++){
+            $timeID[$i][$j] = $times[$i][$j]->id2;
+        }
 
- function minTimeLength(int $minTime) : int{
-    return $minTime / 15;
+    }
+    echo 'done';
+    return $timeID;
 }
 
+
+
+//calculate number based on minimal time
+//used in analysis
+ function minTimeLength(int $minTime) : int {
+    return intval($minTime / 15);
+}
+
+//transforms 2d array to 1d array
+//contains unique id2 values
+function getUnique1D(array $input) : array {
+    $output = array();
+    for($i = 0; $i < count($input); $i++){
+        for($j = 0; $j < count($input[$i]); $j++){
+            $output[] = $input[$i][$j];
+        }
+    }
+    $unique = array_unique($output);                //maybe problem bc of array_unique
+    return $unique;
+}
+
+//checks if id from uniqueID is contained in timeID
+//for every id => array of 0 and 1
+//true => 1 (id in array)
+//false => 0 (id not in array)
+function convertID(array $timeID, array $uniqueID) : array {
+    $convertedID[][] = array();
+    for($i = 0; $i < count($uniqueID); $i++){
+        $id = $uniqueID[$i];
+        for($j = 0; $j < count($timeID); $j++){
+            if(in_array($id, $timeID[$j]) == true){
+                $convertedID[$i][$j] = 1;
+            }else{
+                $convertedID[$i][$j] = 0;
+            }
+        }
+
+    }
+    return $convertedID;
+}
+
+//checks availabilty based on minTime and the converted ID array (output convertID())
+function checkTime(int $minTime, array $uniqueID, array $convertedID) : array {
+    $mind = minTimeLength($minTime);
+    $output = array();
+    for($i = 0; $i < count($convertedID); $i++){
+        $count = 0;
+        $free = new FreeRooms();
+        for($j = 0; $j < count($convertedID[$i]); $i++){
+            if($convertedID[$i][$j] == 1){
+                $count++;
+            }else{
+                if($count >= $mind){
+                    $free->id = $uniqueID[$i]; //because convertedID and uniqueID have same structure
+                    $free->$start = $j - $count;
+                    $free->$end = $j - 1;
+                }else{
+                    $count = 0;
+                    continue;
+                }
+            }
+        }
+        $output[$i] = $free;
+    }
+    return $output;
+    echo 'done2';
+}
+
+
+
+//Test
+//later values GUI interaction
+$start = new DateTime("Wednesday next week 8:00:00");
+$end = new DateTime("Wednesday next week 10:00:00");
+$minTime = minTimeLength(60);
+
+//overall problems may caused by empty room objects(veranstaltungsort missing)
+$timeID = getIDarray($start, $end);//check  
+$uniqueID = getUnique1D($timeID);//check
+$convertedID = convertID($timeID, $uniqueID);//check
+$freerooms = checkTime($minTime, $uniqueID, $convertedID);//problem
+// print_r($freerooms);
 
