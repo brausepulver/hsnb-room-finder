a403a89d4d81393f8938421ea61c48dd202a07d8
brausepulver <34420739+brausepulver@users.noreply.github.com>
2020-04-13 13:33:57 +0200

    Fix require_once in analyse, add AnalysisTest

--- src/analysis/analyse.php
+++ src/analysis/analyse.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once "../Import/Importer.php";
+require_once(__DIR__ . '/../Import/Importer.php');
 
 use Import\Importer;
 use Import\Importer\{Event, Room, TimeVector};
