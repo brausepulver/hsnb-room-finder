4a7664325143fcf73128d087750912f7fa956362
brausepulver <34420739+brausepulver@users.noreply.github.com>
2020-05-25 15:58:39 +0200

    Add back previously removed search button.

--- public/form.php
+++ public/form.php
@@ -86,6 +86,9 @@ for ($i = 1, $roomTypes = Importer::getRoomTypes(); $i < count($roomTypes); $i++
 ?>
                     </select>
                 </li>
+                <li>
+                    <input type="submit" value="Suchen">
+                </li>
             </ul>
         </form>
     </section>
