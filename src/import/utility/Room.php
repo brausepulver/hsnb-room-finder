40ac61195e2157f9f4446c17ed9ccce414bf197c
brausepulver <34420739+brausepulver@users.noreply.github.com>
2020-05-28 16:39:34 +0200

    Add documentation to getAvailableTimeFrames method.

--- src/import/utility/Room.php
+++ src/import/utility/Room.php
@@ -43,26 +43,41 @@ class Room
         $interval = new \DateInterval('PT15M');
         $v = new TimeVector($start, $finish, $interval, true);
 
+        /* Wenn keine Events für den Raum gespeichert wurden, ist occupiedTimeFrames leer.
+           Ich gebe also die Start- und End-Zeit der Anfrage zurück. (z.b. 08:00 bis 20:00 Uhr) */
         if (count($this->occupiedTimeFrames) === 0) {
             return [ [clone $start, clone $finish] ];
         }
+        /* Das Feld in der TimeVector-Variable $v wurde für jeden 15-Minuten-Index standardmäßig auf true gesetzt.
+           Jetzt laufe ich alle registrierten Events ($timeFrames) durch, 
+           und setze für jedes Event alle Indizes in $v auf falsch, die das Event zeitlich in dem Raum einnimmt. */
         foreach ($this->occupiedTimeFrames as $timeFrame) {
             for ($i = clone $timeFrame[0]; $i < $timeFrame[1]; $i->add($interval)) {
                 $v->set($i, false);
             }
         }
+        /* $available ist ein Feld der Form [bool], z.B. [true, false, true, false], 
+           entsprechend der Information, ob der Raum um 8:00, 8:15, 8:30, 8:45 Uhr, etc. verfügbar ist. */
         $available = $v->toArray(); $availableOn = [];
 
+        /* Aus dem [bool] $available wird jetzt ein Array der Form [ [DateTime $start, DateTime $ende], ... ] gemacht.
+           Die Variable des Arrays ist $availableOn, und wird später (für die Verarbeitung in query.php) von der Funktion zurückgegeben.
+           Dazu werden alle Zeit-Indizes in $available durchlaufen. */
         for ($i = 0, $j; $i < count($available); $i++) {
+            // Überprüfen, ob $availableOn leer ist. D.h. ob es sich um das erste einzufügende Element handelt.
             $s = count($availableOn) > 0 ? count($availableOn[count($availableOn)-1]) : 0;
 
+            // Wenn der Raum zu der aktuellen Zeit verfügbar ist, füge ein Array der Form [DateTime $start] an $availableOn an.
             if ($available[$i]) {
-                if ($s === 2 || $s == 0) {
+                if ($s === 2 || $s === 0) {
                     $availableOn[] = [$this->indexToTime($start, $i)];
+                    // $j wird hochgezählt und später als DateTime $ende an das Array in der vorherigen Zeile angefügt.
                     $j = $this->indexToTime($start, $i);
                 }
                 $j->add($interval);
             }
+            /* Überprüfen, ob der Raum nicht mehr verfügbar ist, aber es vorher war (es wurde ein [DateTime $start] angelegt),
+               oder ob es sich bei dem Zeit-Index $i um die letzte Zeit in $available handelt (es muss ein DateTime $ende angefügt werden). */
             if ((!$available[$i] || $i === count($available)-1) && $s === 1) {
                 $availableOn[count($availableOn)-1][1] = $j;
             }
